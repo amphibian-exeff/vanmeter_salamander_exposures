b49c6a0c4a1b00aba03e0dc75e2984ea5ce9ee82
Tom Purucker <purucker.tom@gmail.com>
2021-04-09 12:55:02 -0400

    importing gsh data

---
+++ src/00setup.R
@@ -0,0 +1,33 @@
+#Install and load supporting libraries.
+print(Sys.info()[4])
+
+print("list of loaded packages: ")
+print((.packages()))
+
+#tom epa windows
+if(Sys.info()[4]=="DZ2626UTPURUCKE"){
+  rvm_root <- file.path("c:", "git", "rvm_salamander_gsh")
+}
+if(Sys.info()[4]=="LZ2626UTPURUCKE"){
+  rvm_root <- file.path("c:","git","rvm_salamander_gsh")
+}
+
+print(paste("Root directory location: ", rvm_root, sep=""))
+
+rvm_data_in <- file.path(rvm_root, "data_in")
+rvm_data_out <- file.path(rvm_root, "data_out")
+rvm_graphics <- file.path(rvm_root, "graphics")
+
+#check to see if directories are accessible
+boo = file.exists(file.path(rvm_data_in,"/Final_GSH_Salamanders_April_2020.csv"))
+print(paste("check to see if R can access GSF file OK: ", boo))
+
+#GSH units = nM/mL
+#Weight (g)
+#SVL (mm)
+
+rvm_gsh <- read.csv(file.path(rvm_csv_in,"/Final_GSH_Salamanders_April_2020.csv"), stringsAsFactors = TRUE)
+
+summary(rvm_gsh)
+colnames(rvm_gsh)
+
